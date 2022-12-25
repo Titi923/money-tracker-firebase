2d4e2db93459d36655ae75c14d55d890dce1ccc4
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-25 23:31:52 +0200

    First Commit & Getting Everything Ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-   
+      <h1>Text</h1>
+      <button className='btn'>ASDAS</button>
     </div>
   );
 }
