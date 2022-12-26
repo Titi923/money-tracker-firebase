51ffe6c3c11d9c0f2ab38c6e034bff619563ec8c
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-26 14:38:36 +0200

    Creating the pages and implementing the routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Signup from "./pages/signup/Signup"
+
 function App() {
+  
   return (
     <div className="App">
-      <h1>Text</h1>
-      <button className='btn'>ASDAS</button>
+      <BrowserRouter>
+        
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
