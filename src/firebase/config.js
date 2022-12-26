89072b4104a242a3084d18b6bfd426b5584c5913
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-26 21:07:33 +0200

    Firebase Authentication Setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeYVVITAzALMCamQW_Z6jaAFUIzIHjmpw",
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig)
 
 // init servise
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 
-export { projectFirestore }
+export { projectFirestore, projectAuth }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,8 +5,7 @@ import styles from './Home.module.css'
 export default function Home() {
   return (
     <div>
-      {console.log(app)}
-
+      Home
     </div>
   )
 }
