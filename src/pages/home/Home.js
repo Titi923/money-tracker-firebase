d20efe545d0ee338f902f6725b55cd0da938aa5f
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-30 23:21:23 +0200

    Making a Transaction Form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,18 @@
-
 // styles
 import styles from './Home.module.css'
 
+// components
+import TransactionForm from './TransactionForm'
+
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>
+        Transaction List
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   )
 }
