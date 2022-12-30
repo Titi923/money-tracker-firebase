914ccedc635ee1ddb8d47aa74690d4b90914d7c2
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-30 23:59:42 +0200

    Firestore Timestamp

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,19 +3,22 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeYVVITAzALMCamQW_Z6jaAFUIzIHjmpw",
-  authDomain: "moneyflow-9c2ce.firebaseapp.com",
-  projectId: "moneyflow-9c2ce",
-  storageBucket: "moneyflow-9c2ce.appspot.com",
-  messagingSenderId: "598549972405",
-  appId: "1:598549972405:web:05e0b23bf66733f05645fc"
+  apiKey: 'AIzaSyBeYVVITAzALMCamQW_Z6jaAFUIzIHjmpw',
+  authDomain: 'moneyflow-9c2ce.firebaseapp.com',
+  projectId: 'moneyflow-9c2ce',
+  storageBucket: 'moneyflow-9c2ce.appspot.com',
+  messagingSenderId: '598549972405',
+  appId: '1:598549972405:web:05e0b23bf66733f05645fc'
 };
 
 // init firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // init servise
-const projectFirestore = firebase.firestore()
-const projectAuth = firebase.auth()
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-export { projectFirestore, projectAuth }
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
